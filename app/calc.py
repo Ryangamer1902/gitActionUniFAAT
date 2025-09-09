f8b51b55fdb1f4c39f937d10a8c1862215d1f284
Vinidespk <viniciusakutagawa24@gmail.com>
2025-09-08 21:26:09 -0300

    feat: adiciona função potencia

--- app/calc.py
+++ app/calc.py
@@ -1,2 +1,15 @@
+def soma(a, b):
+    # return a - b  # ERRO proposital
+    return a + b  # Correção
+
+def subtracao(a, b):
+    return a - b
+
+def multiplica(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
 def potencia(base, expoente):
     return base ** expoente
